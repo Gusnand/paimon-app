90287721dc03ac2ed4e0fce2a5ba80d0aded17f6
hoshiirui <shibaharumii@gmail.com>
2024-12-18 13:33:22 +0800

    fixing tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import {
+  Quicksand_300Light,
+  Quicksand_400Regular,
+  Quicksand_600SemiBold,
+  Quicksand_700Bold,
+} from "@expo-google-fonts/quicksand";
+import { Text } from "react-native";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Quicksand_400Regular,
+    Quicksand_600SemiBold,
+    Quicksand_700Bold,
+    Quicksand_300Light,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading fonts...</Text>;
+  }
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
---
+++ constants/Fonts.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const FontStyles = StyleSheet.create({
+  quicksand: {
+    fontSize: 20,
+    fontFamily: "Quicksand_300Light",
+  },
+});
