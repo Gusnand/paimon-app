ac307bb152b7e47be190736db83eccb35d0747fc
hoshiirui <shibaharumii@gmail.com>
2024-12-18 13:05:36 +0800

    completing tabs template

---
+++ app/(tabs)/destination.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function destination() {
+  return (
+    <View>
+      <Text>destination</Text>
+    </View>
+  );
+}
---
+++ constants/Colors.ts
@@ -0,0 +1,22 @@
+const tintColorLight = "#7f57f1";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  light: {
+    text: "#3C3C3C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#F1737B",
+    tabIconDefault: "#F1737B",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9ba1a6",
+    tabIconDefault: "9ba1a6",
+    tabIconSelected: tintColorDark,
+  },
+  PRIMARY: "#F1737B",
+};
