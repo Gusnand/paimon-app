befdfe93b5bb787e59ab41176e42259fcd502d2b
hoshiirui <shibaharumii@gmail.com>
2024-12-19 19:19:48 +0800

    finishing home hard coded

---
+++ components/Home/WisataTerpopuler.tsx
@@ -0,0 +1,67 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { FontStyles } from "@/constants/Fonts";
+import { Colors } from "@/constants/Colors";
+
+export default function WisataTerpopuler() {
+  return (
+    <View style={{ marginVertical: 24 }}>
+      <Text style={[FontStyles.quicksandHeader2Page, { marginBottom: 16 }]}>
+        Wisata Terpopuler
+      </Text>
+      <View style={{ display: "flex", gap: 16 }}>
+        <TouchableOpacity
+          style={{
+            paddingHorizontal: 16,
+            paddingVertical: 24,
+            backgroundColor: Colors.PRIMARYLighter,
+            borderRadius: 12,
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Text style={[FontStyles.quicksandBold, { fontSize: 24 }]}>
+            Jembatan Sakenan
+          </Text>
+          <Text style={[FontStyles.quicksandBold, { fontSize: 14 }]}>
+            Denpasar
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            paddingHorizontal: 16,
+            paddingVertical: 24,
+            backgroundColor: Colors.PRIMARYLighter,
+            borderRadius: 12,
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Text style={[FontStyles.quicksandBold, { fontSize: 24 }]}>
+            Dharmanegara Alaya
+          </Text>
+          <Text style={[FontStyles.quicksandBold, { fontSize: 14 }]}>
+            Denpasar
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            paddingHorizontal: 16,
+            paddingVertical: 24,
+            backgroundColor: Colors.PRIMARYLighter,
+            borderRadius: 12,
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Text style={[FontStyles.quicksandBold, { fontSize: 24 }]}>
+            Dharmanegara Alaya
+          </Text>
+          <Text style={[FontStyles.quicksandBold, { fontSize: 14 }]}>
+            Denpasar
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
