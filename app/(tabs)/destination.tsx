4c8ba153dcc87c3d83ad45984379e1c58c1de1f5
hoshiirui <shibaharumii@gmail.com>
2024-12-20 11:58:56 +0800

    damn

--- app/(tabs)/destination.tsx
+++ app/(tabs)/destination.tsx
@@ -1,24 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Header from "@/components/Layout/Header";
-import MapView from "react-native-maps";
 
 export default function destination() {
   return (
     <View style={{ flex: 1, margin: 24 }}>
       {/* header */}
       <Header headerText="Wisata" />
-      <View style={{ flex: 1 }}>
-        <MapView
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          style={{ width: "100%", height: "100%" }}
-        />
-      </View>
+      <View style={{ flex: 1 }}></View>
     </View>
   );
 }
